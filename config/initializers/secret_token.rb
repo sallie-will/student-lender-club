06d36e7e071548e15a12420f8a8c310f917001f0
Rosie Hoyem <rosiehoyem@gmail.com>
2014-03-03 01:09:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudentLenderClub::Application.config.secret_key_base = '95ff6e0c0aedf5bf5dcb5747afcfb35ead4e95b52ed2d5a428f6f3e4e8ce3be5c91e0b5d7014024a1e1bf556a9fae08b0964bd707211d4f8b62501e26e684799'
