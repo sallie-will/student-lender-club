7e19b6323fa3fa0283d327fc9f52fa23796e005c
Rosie Hoyem <rosiehoyem@gmail.com>
2014-03-03 01:23:41 -0500

    added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 StudentLenderClub::Application.routes.draw do
+  get "pages/index"
+  get "pages/about"
   devise_for :users
   resources :users
 
@@ -6,7 +8,7 @@ StudentLenderClub::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
