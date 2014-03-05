e775bce18c547bb7e83c7912f73b01ffbeba0140
Chris Scott <Chris@gnmlbs.com>
2014-03-05 01:45:21 -0500

    removed weird html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,39 +12,12 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= <div class="slider-inner">
-<div id="da-slider" class="da-slider">
-    <div class="da-slide da-slide-current">
-        <h2><i>CLEAN &amp; FRESH</i> <br> <i>FULLY RESPONSIVE</i> <br> <i>DESIGN</i></h2>
-            <p><i>Lorem ipsum dolor amet</i> <br> <i>tempor incididunt ut</i> <br> <i>veniam omnis </i></p>
-                <div class="da-img"><img src="assets/plugins/parallax-slider/img/1.png" alt=""></div>
-                </div>
-                <div class="da-slide">
-                    <h2><i>RESPONSIVE VIDEO</i> <br> <i>SUPPORT AND</i> <br> <i>MANY MORE</i></h2>
-                        <p><i>Lorem ipsum dolor amet</i> <br> <i>tempor incididunt ut</i></p>
-                            <div class="da-img">
-                                <iframe src="http://player.vimeo.com/video/47911018" width="530" height="300" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
-                            </div>
-                        </div>
-                        <div class="da-slide">
-                            <h2><i>USING BEST WEB</i> <br> <i>SOLUTIONS WITH</i> <br> <i>HTML5/CSS3</i></h2>
-                                <p><i>Lorem ipsum dolor amet</i> <br> <i>tempor incididunt ut</i> <br> <i>veniam omnis </i></p>
-                                    <div class="da-img"><img src="assets/plugins/parallax-slider/img/html5andcss3.png" alt="image01"></div>
-                                    </div>
-                                    <div class="da-arrows">
-                                        <span class="da-arrows-prev"></span>
-                                        <span class="da-arrows-next"></span>
-                                    </div>
-                                    <nav class="da-dots"><span class="da-dots-current"></span><span></span><span></span></nav></div><!--/da-slider-->
-                                </div>
-
-                            //= require turbolinks
+//= require turbolinks
 //= require bootstrap
 //= require back-to-top
 //= require jquery-migrate-1.2.1.min
 //= require jquery.cslider
 //= require jquery.flexslider.js
-
 //= require modernizr
 //= require_tree .
 
